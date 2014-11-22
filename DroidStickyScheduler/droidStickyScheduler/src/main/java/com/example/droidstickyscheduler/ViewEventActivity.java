38ec1a43e91bea32e8e69a8b407ef01a09c0e304
Genesis Palaganas <itetsky123@gmail.com>
2014-11-22 20:00:36 +0800

    Added Sticky Scheduler android project

---
+++ DroidStickyScheduler/droidStickyScheduler/src/main/java/com/example/droidstickyscheduler/ViewEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.droidstickyscheduler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ViewEventActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_event);
+		setTitle("November 16");
+	}
+}
